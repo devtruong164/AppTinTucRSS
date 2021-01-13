c049c1ea1c936d178d58b9a54db026273771f311
Truong <dev.truong164@gmail.com>
2021-01-13 13:09:28 +0700

    init

---
+++ app/src/main/java/com/example/appdocbaorss/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.appdocbaorss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lvTieuDe;
+    ArrayList<String> arrayTitle, arrayLink;
+    ArrayAdapter adapter;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvTieuDe = (ListView) findViewById(R.id.listviewTieuDe);
+        arrayTitle = new ArrayList<String>();
+        arrayLink = new ArrayList<>();
+        adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, arrayTitle);
+
+        lvTieuDe.setAdapter(adapter);
+
+        new ReadRSS().execute("https://vnexpress.net/rss/the-gioi.rss");
+        new ReadRSS().execute("https://vnexpress.net/rss/the-thao.rss");
+        new ReadRSS().execute("https://vnexpress.net/rss/so-hoa.rss");
+        lvTieuDe.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+                intent.putExtra("linkTinTuc", arrayLink.get(position));
+                startActivity(intent);
+            }
+        });
+    }
+
+    private class ReadRSS extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... string) {
+            StringBuilder content = new StringBuilder();
+            try {
+                URL url = new URL(string[0]);
+                InputStreamReader inputStreamReader = new InputStreamReader(url.openConnection().getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                String line = "";
+                while ((line = bufferedReader.readLine()) != null) {
+                    content.append(line);
+
+                }
+                bufferedReader.close();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return content.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            XMLDOMParser parser = new XMLDOMParser();
+            Document document = parser.getDocument(s);
+            NodeList nodeList = document.getElementsByTagName("item");
+            String tieuDe = "";
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Element element = (Element) nodeList.item(i);
+                tieuDe = parser.getValue(element, "title");
+                arrayTitle.add(tieuDe);
+                arrayLink.add(parser.getValue(element,"link"));
+            }
+            adapter.notifyDataSetChanged();
+        }
+    }
+}
